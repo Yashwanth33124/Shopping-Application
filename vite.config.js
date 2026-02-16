d09133f1f99138b0805c67ae5cd0040cc2614d43
Yashwanth33124 <cheruvuyashwanth99@gmail.com>
2026-02-16 10:39:39 +0530

    Added GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "/Shopping-Application/",   // 👈 VERY IMPORTANT
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
