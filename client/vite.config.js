a2192583721d6a1572a195d77ece7467af34b124
Yashwanth33124 <cheruvuyashwanth99@gmail.com>
2026-06-01 22:26:51 +0530

    fix: dynamically set base path for github pages deployment vs vercel root

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
-  base: "/",
+  base: process.env.NODE_ENV === 'production' && !process.env.VERCEL ? "/Shopping-Application/" : "/",
 
   plugins: [
     react(),
